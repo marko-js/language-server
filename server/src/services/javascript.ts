9b84dd2de876f4d4506794e6390c08f5ae479cf5
Andrew Gliga <agliga@ebay.com>
2019-08-12 09:32:40 -0700

    feat: javascript autocomplete and fixed issues with style
    * Added initial javascript checks
    
    * Added javascript syntax and fixed css/less

---
+++ server/src/services/javascript.ts
@@ -0,0 +1,84 @@
+import * as fs from 'fs';
+import { getFileFsPath, getFilePath } from '../util/paths'
+import * as ts from 'typescript';
+import * as tsModule from 'typescript';
+import { TextDocument } from 'vscode-languageserver';
+import { IAutocompleteArguments } from '../util/autocomplete';
+// import { getSingleTypeDocument } from '../util/document';
+// import { LanguageService } from 'vscode-css-languageservice';
+
+// TODO add initial files
+const scriptFileNameSet = new Set();
+const localScriptRegionDocuments = new Map<string, TextDocument>();
+let currentScriptDoc: TextDocument;
+
+function getDefaultCompilerOptions() {
+    const defaultCompilerOptions: ts.CompilerOptions = {
+        allowNonTsExtensions: true,
+        allowJs: true,
+        lib: ['lib.dom.d.ts', 'lib.es2017.d.ts'],
+        target: tsModule.ScriptTarget.Latest,
+        moduleResolution: tsModule.ModuleResolutionKind.NodeJs,
+        module: tsModule.ModuleKind.CommonJS,
+        jsx: tsModule.JsxEmit.Preserve,
+        allowSyntheticDefaultImports: true,
+        experimentalDecorators: true
+    };
+
+    return defaultCompilerOptions;
+}
+
+export function getLanguageHost(rootFileNames: string[]): ts.LanguageServiceHost {
+    const files: ts.MapLike<{ version: number }> = {};
+
+    return {
+        getScriptFileNames: () => rootFileNames,
+        getScriptVersion: fileName =>
+            files[fileName] && files[fileName].version.toString(),
+        getScriptSnapshot: fileName => {
+            if (!fs.existsSync(fileName)) {
+                return undefined;
+            }
+
+            return ts.ScriptSnapshot.fromString(fs.readFileSync(fileName).toString());
+        },
+        getCurrentDirectory: () => process.cwd(),
+        getCompilationSettings: () => getDefaultCompilerOptions(),
+        getDefaultLibFileName: options => ts.getDefaultLibFilePath(options),
+        fileExists: ts.sys.fileExists,
+        readFile: ts.sys.readFile,
+        readDirectory: ts.sys.readDirectory
+    }
+};
+
+export function updateCurrentMarkoTextDocument(options:IAutocompleteArguments) {
+    const {
+        doc,
+        // scopeAtPos,
+
+    } = options;
+    const fileFsPath = getFileFsPath(doc.uri);
+    const filePath = getFilePath(doc.uri);
+    // When file is not in language service, add it
+    if (!localScriptRegionDocuments.has(fileFsPath)) {
+        if (fileFsPath.endsWith('.marko')) {
+            scriptFileNameSet.add(filePath);
+        }
+    }
+
+    // if (!currentScriptDoc || doc.uri !== currentScriptDoc.uri || doc.version !== currentScriptDoc.version) {
+    //     currentScriptDoc = getSingleTypeDocument(doc, scopeAtPos.regions, 'script');
+    //     const localLastDoc = localScriptRegionDocuments.get(fileFsPath);
+    //     if (localLastDoc && currentScriptDoc.languageId !== localLastDoc.languageId) {
+    //         // if languageId changed, restart the language service; it can't handle file type changes
+    //         jsLanguageService.dispose();
+    //         jsLanguageService = tsModule.createLanguageService(jsHost);
+    //     }
+    //     localScriptRegionDocuments.set(fileFsPath, currentScriptDoc);
+    //     // versions.set(fileFsPath, (versions.get(fileFsPath) || 0) + 1);
+    // }
+    return {
+        // service: jsLanguageService,
+        scriptDoc: currentScriptDoc
+    };
+}
--- server/src/util/autocomplete.ts
+++ server/src/util/autocomplete.ts
@@ -1,7 +1,11 @@
 import { createParser } from "htmljs-parser";
 import { ScopeType, Scope, IHtMLJSParserEvent } from "./marko";
 import { CompletionList, TextDocument, Position } from "vscode-languageserver";
-import { getStyleAutocomplete } from "./stylesheet";
+
+import { getStyleAutocomplete, supportedCSSList } from "./stylesheet";
+import { getJSAutocomplete } from "./javascript";
+import { EmbeddedRegion, LanguageId, RegionType } from "./document";
+
 const DEBUG = process.env.DEBUG === 'true' || false;
 
 export interface IAutocompleteArguments {
@@ -42,9 +46,60 @@ export function generateAutocomplete(attributes: { [key: string]: IMarkoAttribut
     return ret;
 }
 
+function getStyleType(event: IHtMLJSParserEvent): LanguageId {
+    let found: LanguageId;
+    try {
+        event.shorthandClassNames.forEach((e) => {
+            e.rawParts.forEach((raw) => {
+                if (!found && supportedCSSList.includes(raw.text)) {
+                    found = raw.text as LanguageId;
+                }
+            });
+        });
+    } catch (e) {
+        // Default to css
+        found = 'css';
+    }
+    return found;
+}
+
+
+
+function createRegion(region: EmbeddedRegion[], event: IHtMLJSParserEvent): EmbeddedRegion[] {
+    let languageId: LanguageId = null;
+    let type: RegionType = null;
+    if (event.tagName === 'style') {
+        languageId = getStyleType(event);
+        type = 'style';
+    } else if (event.tagName === 'class') {
+        languageId = 'javascript';
+        type = 'script';
+    } else if (event.type === 'placeholder') {
+        type = 'script';
+        languageId = 'javascript'
+    }
+    if (languageId !== null) {
+        region.push({
+            start: event.pos,
+            end: event.endPos,
+            type,
+            languageId
+
+        })
+    }
+    return region;
+
+}
+
 export async function getAutocomleteAtText(offset: number, text: string) {
     let found: boolean = false;
-    return new Promise(function (resolve: (tag: Scope | boolean) => any) {
+    const regions: EmbeddedRegion[] = [];
+    let resolveData: Scope = {
+        tagName: '',
+        scopeType: ScopeType.NO_SCOPE,
+        regions
+    };
+    return new Promise(function (resolve: (tag: Scope) => any) {
         const parser = createParser({
             onError: (error: any, data: any) => {
                 resolve({
@@ -60,7 +115,9 @@ export async function getAutocomleteAtText(offset: number, text: string) {
                     tagName,
                     tagNameEndPos,
                     attributes,
+                    argument,
                 } = event;
+                createRegion(regions, event);
 
                 // Don't process when the offset is not inside a tag or we found our tag already
                 if (checkPosition(found, event, offset)) return;
@@ -74,11 +131,24 @@ export async function getAutocomleteAtText(offset: number, text: string) {
                 };
 
                 if (offset <= tagNameEndPos) {
-                    return resolve({
-                        tagName,
-                        scopeType: ScopeType.TAG,
-                        event,
-                    });
+                    if (offset > argument.pos && offset <= argument.endPos) {
+                        // resolve as argument
+                        resolveData = {
+                            tagName,
+                            scopeType: ScopeType.JAVASCRIPT,
+                            event,
+                            regions,
+                        };
+                    } else {
+                        // Check and see if there is an argument
+                        resolveData = {
+                            tagName,
+                            scopeType: ScopeType.TAG,
+                            event,
+                            regions,
+                        };
+                    }
+                    return;
                 }
 
                 for (const attribute of attributes) {
@@ -93,31 +163,60 @@ export async function getAutocomleteAtText(offset: number, text: string) {
                     if (offset > attribute.pos && offset <= attribute.endPos) {
                         // user is inside attr
                         if (!attribute.value && !attribute.argument) {
-                            return resolve({
+                            resolveData = {
                                 tagName,
                                 data: attribute.name,
                                 scopeType: ScopeType.ATTR_NAME,
                                 event,
-                            });
+                                regions,
+                            };
+
                         } else if (attribute.argument) {
 
-                            return resolve({
+                            resolveData = {
                                 tagName,
+                                regions,
                                 data: attribute.argument.value.slice(1, -1),
                                 scopeType: ScopeType.ATTR_VALUE,
                                 event,
-                            })
+                            };
                         } else {
-                            return resolve({
+                            resolveData = {
                                 tagName,
+                                regions,
                                 data: attribute.value,
-                                scopeType: ScopeType.ATTR_VALUE,
+                                scopeType: ScopeType.JAVASCRIPT,
                                 event,
-                            })
+                            };
                         }
                     }
                 }
                 // return resolve(defaultTagScope);
+            },
+            onScriptlet: (event: IHtMLJSParserEvent) => {
+                createRegion(regions, event);
+                if (checkPosition(found, event, offset)) return;
+                resolveData = {
+                    tagName: null,
+                    regions,
+                    data: event.value,
+                    scopeType: ScopeType.JAVASCRIPT,
+                    event,
+                };
+
+
+            },
+            onPlaceholder: (event: IHtMLJSParserEvent) => {
+                createRegion(regions, event);
+                if (checkPosition(found, event, offset)) return;
+                resolveData = {
+                    tagName: null,
+                    regions,
+                    data: event.value,
+                    scopeType: ScopeType.JAVASCRIPT,
+                    event,
+                };
+
             },
             onCloseTag: (event: IHtMLJSParserEvent) => {
                 const {
@@ -127,12 +226,13 @@ export async function getAutocomleteAtText(offset: number, text: string) {
                 // Don't process when the offset is not inside a tag or we found our tag already
                 if (checkPosition(found, event, offset)) return;
                 found = true;
-                return resolve({
+                resolveData = {
                     tagName,
+                    regions,
                     data: tagName,
                     scopeType: ScopeType.CLOSE_TAG,
                     event,
-                })
+                };
             },
             //  onString: (event: any) => {
             //     if (checkPosition(found, event, offset)) return;
@@ -147,7 +247,8 @@ export async function getAutocomleteAtText(offset: number, text: string) {
             onfinish: () => {
                 DEBUG && console.log("================Finished!!!==============");
                 // TODO: Maybe this is not right? we need it to resolve somehow
-                if (!found) resolve(false);
+                resolve(resolveData);
+                // if (!found) resolve(false);
             }
         });
         parser.parse(text);
@@ -176,14 +277,20 @@ function getHTMLAttrAutiocomplete(options: IAutocompleteArguments): CompletionLi
 }
 
 export function getAttributeAutocomplete(options: IAutocompleteArguments): CompletionList {
-    switch(options.scopeAtPos.tagName) {
+    switch (options.scopeAtPos.tagName) {
         case 'style':
             return getStyleAutocomplete(options);
         case 'class':
-            return null;
+            return getJSAutocomplete(options);
         default:
             return getHTMLAttrAutiocomplete(options);
     }
+}
+
+export function getJavascriptAutocomplete(options: IAutocompleteArguments): CompletionList {
+    // const newDocument = getSingleTypeDocument(options.doc, 'javascript',);
+    // createLanguageService()
+    return getJSAutocomplete(options);
 }
 
 export function getCloseTagAutocomplete(options: IAutocompleteArguments): CompletionList {
