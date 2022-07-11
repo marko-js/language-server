d7dc9a04e2f171db6d375572874428deb3d456b1
Dylan Piercey <dpiercey@ebay.com>
2022-07-11 15:27:27 -0700

    chore: add tests for color provider

---
+++ clients/vscode/src/__tests__/colors.test.ts
@@ -0,0 +1,39 @@
+import vscode from "vscode";
+import snap from "mocha-snap";
+import { getTestDoc, updateTestDoc } from "./setup.test";
+
+describe("document color", () => {
+  it("css color", async () => {
+    await snap.inline(
+      () =>
+        documentColor(
+          `style {
+  body {
+    color: blue;
+  }
+}`
+        ),
+      `rgb(0, 0, 255)
+#0000ff
+hsl(240, 100%, 50%)
+hwb(240 0% 0%)`
+    );
+  });
+});
+
+async function documentColor(src: string) {
+  const doc = getTestDoc();
+  await updateTestDoc(src);
+  const [info] = await vscode.commands.executeCommand<
+    vscode.ColorInformation[]
+  >("vscode.executeDocumentColorProvider", doc.uri);
+
+  const presentations = await vscode.commands.executeCommand<
+    vscode.ColorPresentation[]
+  >("vscode.executeColorPresentationProvider", info.color, {
+    uri: doc.uri,
+    range: info.range,
+  });
+
+  return presentations.map((it) => it.label).join("\n");
+}
