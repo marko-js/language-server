d0f738403393917033e3ca852e887f7fc414bb59
Dylan Piercey <dpiercey@ebay.com>
2022-07-16 15:13:37 -0700

    feat: autocomplete for tag import shorthand

--- server/src/service/marko/complete/OpenTagName.ts
+++ server/src/service/marko/complete/OpenTagName.ts
@@ -1,16 +1,8 @@
-import path from "path";
-import { URI } from "vscode-uri";
-import {
-  CompletionItemKind,
-  CompletionItem,
-  InsertTextFormat,
-  MarkupKind,
-  TextEdit,
-} from "vscode-languageserver";
-import type { TagDefinition } from "@marko/babel-utils";
 import { type Node, NodeType } from "../../../utils/parser";
 import { getDocFile } from "../../../utils/doc-file";
 import type { CompletionMeta, CompletionResult } from ".";
+import getTagNameCompletion from "../util/get-tag-name-completion";
+import type { CompletionItem } from "vscode-languageserver";
 
 export function OpenTagName({
   document,
@@ -18,86 +10,59 @@ export function OpenTagName({
   parsed,
   node,
 }: CompletionMeta<Node.OpenTagName>): CompletionResult {
-  const currentTemplateFilePath = getDocFile(document);
+  const importer = getDocFile(document);
   const tag = node.parent;
-  const tagNameLocation = parsed.locationAt(node);
-  let tags: TagDefinition[];
+  const range = parsed.locationAt(node);
+  const isAttrTag = tag.type === NodeType.AttrTag;
+  const result: CompletionItem[] = [];
 
-  if (tag.type === NodeType.AttrTag) {
+  if (isAttrTag) {
     let parentTag = tag.owner;
     while (parentTag?.type === NodeType.AttrTag) parentTag = parentTag.owner;
     const parentTagDef =
       parentTag && parentTag.nameText && lookup.getTag(parentTag.nameText);
-    tags =
-      (parentTagDef &&
-        parentTagDef.nestedTags &&
-        Object.values(parentTagDef.nestedTags)) ||
-      [];
+
+    if (parentTagDef) {
+      const { nestedTags } = parentTagDef;
+      for (const key in nestedTags) {
+        if (key !== "*") {
+          const tag = nestedTags[key];
+          result.push(
+            getTagNameCompletion({
+              tag,
+              range,
+              importer,
+              showAutoComplete: true,
+            })
+          );
+        }
+      }
+    }
   } else {
-    tags = lookup.getTagsSorted().filter((it) => !it.isNestedTag);
+    const skipStatements = !(
+      tag.concise && tag.parent.type === NodeType.Program
+    );
+    for (const tag of lookup.getTagsSorted()) {
+      if (
+        !(
+          tag.name === "*" ||
+          tag.isNestedTag ||
+          (skipStatements && tag.parseOptions?.statement) ||
+          (tag.name[0] === "_" &&
+            /^@?marko[/-]|[\\/]node_modules[\\/]/.test(tag.filePath))
+        )
+      ) {
+        result.push(
+          getTagNameCompletion({
+            tag,
+            range,
+            importer,
+            showAutoComplete: true,
+          })
+        );
+      }
+    }
   }
 
-  return tags
-    .filter((it) => !it.deprecated)
-    .filter((it) => it.name !== "*")
-    .filter(
-      (it) => /^[^_]/.test(it.name) || !/\/node_modules\//.test(it.filePath)
-    )
-    .map((it) => {
-      let label = it.isNestedTag ? `@${it.name}` : it.name;
-      const fileForTag = it.template || it.renderer || it.filePath;
-      const fileURIForTag = URI.file(fileForTag).toString();
-      const nodeModuleMatch = /\/node_modules\/((?:@[^/]+\/)?[^/]+)/.exec(
-        fileForTag
-      );
-
-      const nodeModuleName = nodeModuleMatch && nodeModuleMatch[1];
-      const isCoreTag = nodeModuleName === "marko";
-
-      const documentation = {
-        kind: MarkupKind.Markdown,
-        value: it.html
-          ? `Built in [<${it.name}>](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/${it.name}) HTML tag.`
-          : nodeModuleName
-          ? isCoreTag
-            ? `Core Marko [<${it.name}>](${fileURIForTag}) tag.`
-            : `Custom Marko tag discovered from the ["${nodeModuleName}"](${fileURIForTag}) npm package.`
-          : `Custom Marko tag discovered from:\n\n[${
-              currentTemplateFilePath
-                ? path.relative(currentTemplateFilePath, fileForTag)
-                : currentTemplateFilePath
-            }](${fileURIForTag})`,
-      };
-
-      if (it.description) {
-        documentation.value += `\n\n${it.description}`;
-      }
-
-      const autocomplete = it.autocomplete && it.autocomplete[0];
-
-      if (autocomplete) {
-        if (autocomplete.displayText) {
-          label = autocomplete.displayText;
-        }
-
-        if (autocomplete.description) {
-          documentation.value += `\n\n${autocomplete.description}`;
-        }
-
-        if (autocomplete.descriptionMoreURL) {
-          documentation.value += `\n\n[More Info](${autocomplete.descriptionMoreURL})`;
-        }
-      }
-
-      return {
-        label,
-        documentation,
-        kind: CompletionItemKind.Class,
-        insertTextFormat: InsertTextFormat.Snippet,
-        textEdit: TextEdit.replace(
-          tagNameLocation,
-          (autocomplete && autocomplete.snippet) || label
-        ),
-      } as CompletionItem;
-    });
+  return result;
 }
