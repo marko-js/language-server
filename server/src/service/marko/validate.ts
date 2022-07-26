c748adcd1595d595e093d4b64fd48ce9fd63b65b
Dylan Piercey <dpiercey@ebay.com>
2022-07-26 14:09:17 -0700

    fix: highlight improvement, avoid invalid diagnostics, deps upgrades.

--- server/src/service/marko/validate.ts
+++ server/src/service/marko/validate.ts
@@ -1,9 +1,10 @@
-import { Diagnostic, DiagnosticSeverity, Range } from "vscode-languageserver";
+import { Diagnostic, DiagnosticSeverity } from "vscode-languageserver";
 import { getCompilerInfo } from "../../utils/compiler";
 import { getDocFile } from "../../utils/doc-file";
 import type { Plugin } from "../types";
 
-const markoErrorRegExp = /^(.+?)(?:\((\d+)(?:\s*,\s*(\d+))?\))?: (.*)$/gm;
+const markoErrorRegExp =
+  /^(.+?)\.marko(?:\((\d+)(?:\s*,\s*(\d+))?\))?: (.*)$/gm;
 
 export const doValidate: Plugin["doValidate"] = (doc) => {
   const fsPath = getDocFile(doc);
@@ -18,22 +19,32 @@ export const doValidate: Plugin["doValidate"] = (doc) => {
       code: false,
       output: "source",
       sourceMaps: false,
+      babelConfig: {
+        caller: {
+          name: "@marko/language-server",
+          supportsStaticESM: true,
+          supportsDynamicImport: true,
+          supportsTopLevelAwait: true,
+          supportsExportNamespaceFrom: true,
+        },
+      },
     });
   } catch (e) {
     let match: RegExpExecArray | null;
     while ((match = markoErrorRegExp.exec((e as Error).message))) {
-      const [, fileName, rawLine, rawCol, msg] = match;
-      const line = (parseInt(rawLine, 10) || 1) - 1;
-      const col = (parseInt(rawCol, 10) || 1) - 1;
-      diagnostics.push(
-        Diagnostic.create(
-          Range.create(line, col, line, col),
-          msg,
-          DiagnosticSeverity.Error,
-          undefined,
-          fileName
-        )
-      );
+      const [, , rawLine, rawCol, message] = match;
+      const pos = {
+        line: (parseInt(rawLine, 10) || 1) - 1,
+        character: (parseInt(rawCol, 10) || 1) - 1,
+      };
+      diagnostics.push({
+        range: { start: pos, end: pos },
+        source: "marko",
+        code: undefined,
+        tags: undefined,
+        severity: DiagnosticSeverity.Error,
+        message,
+      });
     }
   }
 
