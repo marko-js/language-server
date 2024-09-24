7a9b2cbc2ec31ffaeb63bb8a2ed7c965ceb40f98
Goutham Ponnada <sponnada@ebay.com>
2024-09-24 16:47:07 -0700

    fix(axe-core): disabled experimental rules

--- packages/language-server/src/service/html/axe-rules/rule-exceptions.ts
+++ packages/language-server/src/service/html/axe-rules/rule-exceptions.ts
@@ -1,7 +1,7 @@
 import * as r from "./axe-rules";
 
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-  k: infer I,
+  k: infer I
 ) => void
   ? I
   : never;
@@ -34,7 +34,6 @@ type Blacklist =
   | typeof r.semantics.pageHasHeadingOne
   | typeof r.structure.dlitem
   | typeof r.structure.listitem
-  | typeof r.tables.tdHasHeader
   | typeof r.tables.tdHeadersAttr
   // Seemingly broken in axe-core or JSDom
   | typeof r.aria.ariaRoledescription
@@ -46,14 +45,19 @@ type Blacklist =
   | typeof r.parsing.duplicateId
   | typeof r.parsing.duplicateIdActive
   | typeof r.parsing.duplicateIdAria
-  | typeof r.semantics.pAsHeading
   | typeof r.sensoryAndVisualCues.targetSize
   | typeof r.structure.avoidInlineSpacing
-  | typeof r.structure.cssOrientationLock
-  | typeof r.structure.hiddenContent
   // handled by TypeScript
   | typeof r.aria.ariaValidAttrValue
-  | typeof r.aria.ariaAllowedAttr;
+  | typeof r.aria.ariaAllowedAttr
+  // blacklisted as the rules are in axe-core experimental phase
+  | typeof r.structure.cssOrientationLock
+  | typeof r.keyboard.focusOrderSemantics
+  | typeof r.structure.hiddenContent
+  | typeof r.semantics.labelContentNameMismatch
+  | typeof r.semantics.pAsHeading
+  | typeof r.tables.tableFakeCaption
+  | typeof r.tables.tdHasHeader;
 
 type Whitelist = Exclude<RuleId, Blacklist>;
 
@@ -76,7 +80,6 @@ export const ruleExceptions: { [id in Whitelist]: Exceptions } = {
   [r.forms.autocompleteValid]: {},
   [r.forms.formFieldMultipleLabels]: {},
   [r.keyboard.accesskeys]: {},
-  [r.keyboard.focusOrderSemantics]: {},
   [r.keyboard.frameFocusableContent]: { unknownBody: true },
   [r.keyboard.skipLink]: { unknownBody: true },
   [r.keyboard.tabindex]: {},
@@ -95,7 +98,6 @@ export const ruleExceptions: { [id in Whitelist]: Exceptions } = {
   [r.nameRoleValue.linkName]: { unknownBody: true, attrSpread: true },
   [r.parsing.marquee]: {},
   [r.semantics.identicalLinksSamePurpose]: {},
-  [r.semantics.labelContentNameMismatch]: { unknownBody: true },
   [r.semantics.landmarkNoDuplicateBanner]: {},
   [r.semantics.landmarkNoDuplicateContentinfo]: {},
   [r.semantics.landmarkNoDuplicateMain]: {},
@@ -106,7 +108,6 @@ export const ruleExceptions: { [id in Whitelist]: Exceptions } = {
   [r.structure.list]: { unknownBody: true },
   [r.tables.scopeAttrValid]: {},
   [r.tables.tableDuplicateName]: { unknownBody: true },
-  [r.tables.tableFakeCaption]: { unknownBody: true },
   [r.tables.thHasDataCells]: { unknownBody: true },
   [r.textAlternatives.areaAlt]: { attrSpread: true },
   [r.textAlternatives.documentTitle]: { unknownBody: true },
