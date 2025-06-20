6e930d1bda84ee6bf278cb9f90705b4ea69903e1
AngusMorton <angus.m.morton@gmail.com>
2025-06-20 20:52:30 +1000

    Only modify diagnostics that arent contained within mappings

--- packages/language-server/src/plugins/typescript/diagnostic-enhancements.ts
+++ packages/language-server/src/plugins/typescript/diagnostic-enhancements.ts
@@ -11,6 +11,27 @@ export function enhanceDiagnosticPositions(
     const scriptStartOffset = document.offsetAt(diagnostic.range.start);
     const scriptEndOffset = document.offsetAt(diagnostic.range.end);
 
+    // If the diagnostic is wholly contained within a mapping, then leave it untouched
+    // because it is already correctly positioned.
+    const containedMapping = mappings.find((mapping) => {
+      const generatedStartOffset = mapping.generatedOffsets[0];
+      const generatedEndOffset = generatedStartOffset + mapping.lengths[0];
+      return (
+        scriptStartOffset >= generatedStartOffset &&
+        scriptEndOffset <= generatedEndOffset
+      );
+    });
+
+    if (containedMapping) {
+      return diagnostic;
+    }
+
+    // Otherwise, find the best mapping to adjust the diagnostic position
+    // by calculating the overlap with the generated offsets. The mapping
+    // with the largest overlap will be used to adjust the diagnostic position.
+
+    // This logic is used to ensure that diagnostics are positioned correctly
+    // over tag names and attributes.
     let bestMapping: CodeMapping | null = null;
     let maxOverlap = 0;
 
